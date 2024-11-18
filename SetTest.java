e32ebb15b0c8d5bc90b5c4bccada27464141e4ef
kjellida <156799216+kjellida@users.noreply.github.com>
2024-11-18 16:01:33 +0100

    Add files via upload

---
+++ SetTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetTest {
+
+    @Test
+    void insert() {
+        var set = new Set();
+
+
+    }
+
+    @Test
+    void member() {
+    }
+
+    @Test
+    void intersect() {
+    }
+
+    @Test
+    void distinctClosed() {
+    }
+}
---
+++ WorkScheduleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class WorkScheduleTest {
+
+    @Test
+    public void setRequiredNumber(){
+        WorkSchedule ws = new WorkSchedule(0);
+
+
+    }
+
+}
